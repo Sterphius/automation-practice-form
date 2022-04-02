dd6cc6c0d363c32538f2e8b63c28c07c1fc19505
Sterphius <malyshff@gmail.com>
2022-04-02 23:05:50 +0300

    add Selenide test

---
+++ src/test/java/io/github/sterphius/tests/SelenideWikiCheck.java
@@ -0,0 +1,39 @@
+package io.github.sterphius.tests;
+
+import com.codeborne.selenide.Condition;
+import com.codeborne.selenide.Selenide;
+import org.junit.jupiter.api.Test;
+
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Selectors.*;
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.$$;
+
+/*
+    Вопрос:
+    Есть ли разница между $("h1 div"); и $("h1").$("div"); - может ли привести к тому что, поиск найдёт разные элементы?
+    Если может - приведите пример, когда.
+    Ответ:
+    $("h1 div") - будет искать первый элемент h1 с div внутри
+    $("h1").$("div") - будет искать первый элемент h1 и внутри этого элемента произойдёт попытка найти первый div
+*/
+
+public class SelenideWikiCheck {
+    //Разработайте следующий автотест:
+    //- Откройте страницу Selenide в Github
+    //- Перейдите в раздел Wiki проекта
+    //- Убедитесь, что в списке страниц (Pages) есть страница SoftAssertions
+    //- Откройте страницу SoftAssertions, проверьте что внутри есть пример кода для JUnit5
+
+    String selenideGitHubUrl = "https://github.com/selenide/selenide";
+
+    @Test
+    void expectJUnitExampleAtSelenideWiki(){
+        Selenide.open(selenideGitHubUrl);
+        $(byId("wiki-tab")).click();
+        $(byId("wiki-body")).shouldHave(text("Soft assertions"));
+        $(byText("Soft assertions")).click();
+        $$("h4").findBy(Condition.text("Using JUnit5 extend test class")).sibling(0)
+                .shouldHave(Condition.text("@ExtendWith({SoftAssertsExtension.class})"));
+    }
+}
