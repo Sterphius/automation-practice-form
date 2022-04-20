b252eb1ef8e3722c636c0f4ffe4eb9c4a946d4a7
Sterphius <malyshff@gmail.com>
2022-04-20 14:01:16 +0300

    Drag and Drop

---
+++ src/test/java/io/github/sterphius/tests/DragAndDrop.java
@@ -0,0 +1,23 @@
+package io.github.sterphius.tests;
+
+import com.codeborne.selenide.*;
+import org.junit.jupiter.api.Test;
+
+import static com.codeborne.selenide.Selectors.byId;
+import static com.codeborne.selenide.Selenide.$;
+
+public class DragAndDrop {
+
+    @Test
+    void RealiseDragAndDrop() {
+        Selenide.open("https://the-internet.herokuapp.com/drag_and_drop");
+
+        $(byId("column-a")).shouldHave(Condition.text("a"));
+        $(byId("column-b")).shouldHave(Condition.text("b"));
+
+        $(byId("column-a")).dragAndDropTo($(byId("column-b")));
+
+        $(byId("column-a")).shouldHave(Condition.text("b"));
+        $(byId("column-b")).shouldHave(Condition.text("a"));
+    }
+}
