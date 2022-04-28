6c870f30fce2ddb9eb4dc71039ec6beb309c81ee
Sterphius <malyshff@gmail.com>
2022-04-28 22:25:09 +0300

    rename method to camel case

--- src/test/java/io/github/sterphius/tests/DragAndDrop.java
+++ src/test/java/io/github/sterphius/tests/DragAndDrop.java
@@ -9,7 +9,7 @@ import static com.codeborne.selenide.Selenide.$;
 public class DragAndDrop {
 
     @Test
-    void RealiseDragAndDrop() {
+    void dragAndDrop() {
         Selenide.open("https://the-internet.herokuapp.com/drag_and_drop");
 
         $(byId("column-a")).shouldHave(Condition.text("a"));
